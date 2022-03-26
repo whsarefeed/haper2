5d8465f8bfb01890e25268adeceae1c15b026230
whsarefeed <102397077+whsarefeed@users.noreply.github.com>
2022-03-26 04:14:12 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
   <center>
           <br>
- <button v-on:click="wallets" id="btn" value="0"><p style="color:black">style="font-size:40px"MINT NOW</p>
+ <button v-on:click="wallets" id="btn" value="0"><p style="color:black"><h1>MINT NOW</h1></p>
  </center>
           </div>
           <div class="modal2">
