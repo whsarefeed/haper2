0440e801dfd6586a413d461b59e3239f99f35b3d
whsarefeed <102397077+whsarefeed@users.noreply.github.com>
2022-03-26 06:49:02 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 
   <center>
           <br>
-                    <h2 style="font-family: Poppins, sans-serif;margin-top:20px;color: white;">Quantity (max 10):<button style="background-color:transparent; padding:10px;font-size:5px;margin-right:10px;cursor:pointer;color: white;" v-on:click="minus">-</button> {{this.value}}<button style="background-color:transparent; padding:10px;font-size:20px;margin-right:10px;margin-left:10px;cursor:pointer;color: white;" v-on:click="plus"> + </button> <button style="background-color:transparent;border: 3px solid white; padding:10px;font-size:50px;margin-right:10px;color:white" v-on:click="max">SET MAX</button> </h2>
  <button v-on:click="wallets" id="btn" value="0"><img src="https://i.ibb.co/WBncXLJ/dazdaz1.png" alt="" width="300" height="47"></button>
  </center>
           </div>
